12a039816448b121714344155c0d20f556b3f894
Tanakorn Karode <tanakorn.k@siametrics.com>
2024-06-19 17:10:57 +0700

    Add Google Auth Provider

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
 export const config = {
-    API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api"
+    API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || ""
 }
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import { config } from "@/config";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: config.GOOGLE_CLIENT_ID,
+      clientSecret: config.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
+export default NextAuth(authOptions);
